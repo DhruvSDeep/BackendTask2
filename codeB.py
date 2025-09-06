b1223a5d13b521d741d15c34dbf407acea45333b
DhruvSDeep <114278808+DhruvSDeep@users.noreply.github.com>
2025-09-06 23:57:27 +0530

    Update codeB.py

--- codeB.py
+++ codeB.py
@@ -3,6 +3,7 @@ import csv
 
 mat = []
 
+#Accessing list of types
 typeList = r.get("https://pokeapi.co/api/v2/type").json()['results']
 
 simpleTypeList = []
@@ -19,25 +20,27 @@ for i in range(len(lis)):
     lisEmpty.append("")
 mat.append(lisEmpty)
 
+
+
 for i in typeList:
 
-    if (i['name'] != 'stellar' and i['name'] != 'unknown'):
+    if (i['name'] != 'stellar' and i['name'] != 'unknown'):          #excluding stellar and unknown
         
         tem = [i['name'], '']
         for k in range(len(simpleTypeList)-2):
             tem.append(1)
 
-        a = r.get(i['url']).json()['damage_relations']
+        a = r.get(i['url']).json()['damage_relations']      #supereffective
         for j in a['double_damage_from']:
             col = simpleTypeList.index(j['name']) + 2
             tem[col] = 2
         
 
-        for j in a['half_damage_from']:
+        for j in a['half_damage_from']:                    #not very effective
             col = simpleTypeList.index(j['name']) + 2
             tem[col] = 0.5
 
-        for j in a['no_damage_from']:
+        for j in a['no_damage_from']:                        #immunity
             col = simpleTypeList.index(j['name']) + 2
             tem[col] = 0
 
@@ -49,8 +52,9 @@ for i in typeList:
 
 
 
-    
+    #In the form of a csv for easy visualisation of 2d array as a table
 f = open('rep.csv', 'w+')
 writer = csv.writer(f)
 writer.writerows(mat)
 f.close()
+
